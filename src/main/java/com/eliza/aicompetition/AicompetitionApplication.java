d5cd178d0edac4f617c964b3d9a47a42728122e9
ling0007 <958652631@qq.com>
2026-03-26 21:30:31 +0800

    初始

---
+++ src/main/java/com/eliza/aicompetition/AicompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.eliza.aicompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicompetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliza/aicompetition/AicompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliza.aicompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicompetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
